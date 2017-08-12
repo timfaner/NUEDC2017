3893c6faefa4861b029a9b76051f9c91e828dccb
timfaner <grngrngrngrn@163.com>
2017-08-12 11:28:55 +0800

    test  file

--- Test/test.py
+++ Test/test.py
@@ -1,11 +1,16 @@
-def isaround(x,y,aera_react): #aera_react:(x,y,w,h)
-    try:
-        if x > aera_react[0] and x < aera_react[0] + aera_react[2] \
-        and y > aera_react[1] and y < aera_react[1] + aera_react[3]:
-            return True
-        else:
-            return False
-    except:
+def datapharse(raw):
+    temp = None
+    for i in range(len(raw)):
+        if (raw[-i-2] == 0x55) and (raw[-i-1] == 0x53 )and (-i+8 < 0):
+            temp = raw[-i-2:]
+            break
+    if temp:
+        Xx = (temp[2] | temp[3] << 8)/32768*180
+        Yy = (temp[4] | temp[5] << 8)/32768*180
+        Zz = (temp[6] | temp[7] << 8)/32768*180
+        return (Xx,Yy,Zz)
+    else:
         return False
 
-
+raw = b'US\x8d\xbe\x0b\xeb\x888\x1a\x0e\xd1US\x81\xc4\x8d\xeb\xc4>\x1a\x0e\x8fUS\x91\xcb\xad\xeb\xd7C\x1b\x0e\xdf'
+print(datapharse(raw))
